24bcaf04b1e2c6876dedde7330a40172bcbe0230
Bryce Wray <bwray@wraytx.com>
2019-12-17 20:11:54 -0600

    Testing webpack-dev-server again

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const webpack = require('webpack')
+// const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const Dotenv = require('dotenv-webpack')
 
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,24 @@
 const merge = require("webpack-merge")
+// const webpack = require('webpack')
 const common = require("./webpack.common.js")
+// const WriteFilePlugin = require("write-file-webpack-plugin")
 
 module.exports = merge(common, {
   mode: 'development',
-  /* == uncomment this only if using webpack server instead of Eleventy's Browsersync 
+  /* == uncomment this only if using webpack server instead of Eleventy's Browsersync */
+  /*
   devServer: {
     contentBase: './_site/',
+    overlay: true,
+    hot: true,
     watchContentBase: true
   },
+  plugins: [
+    new WriteFilePlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   */
+  /* == end of webpack server items == */
   watch: true,
   devtool: 'inline-source-map'
 })
