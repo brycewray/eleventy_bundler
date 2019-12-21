a5a3f577cbc8f123781270437d75241d4b75f7d2
Bryce Wray <bwray@wraytx.com>
2019-12-20 18:02:27 -0600

    Similar to last commit plus credit where credit's due re webmentions

--- .eleventy.js
+++ .eleventy.js
@@ -37,6 +37,7 @@ module.exports = function (eleventyConfig) {
   // --and-- https://github.com/planetoftheweb/seven/blob/master/.eleventy.js
   let markdownIt = require("markdown-it")
   let markdownItFootnote = require("markdown-it-footnote")
+  let markdownItPrism = require('markdown-it-prism')
   let markdownItOpts = {
     html: true,
     linkify: true,
@@ -44,6 +45,7 @@ module.exports = function (eleventyConfig) {
   }
   const markdownEngine = markdownIt(markdownItOpts)
   markdownEngine.use(markdownItFootnote)
+  markdownEngine.use(markdownItPrism)
   eleventyConfig.setLibrary("md", markdownEngine)
 
   eleventyConfig.addShortcode("lazypicture", require("./src/assets/utils/lazy-picture.js"))
@@ -62,6 +64,8 @@ module.exports = function (eleventyConfig) {
 
 
   /* === START, webmentions stuff === */
+  // https://mxb.dev/blog/using-webmentions-on-static-sites/
+  // https://sia.codes/posts/webmentions-eleventy-in-depth/
   
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter('head', (array, n) => {
