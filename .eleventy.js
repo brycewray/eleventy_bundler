a867ff3693439d438eb99347ac7d9a83c832690a
Bryce Wray <bwray@wraytx.com>
2019-12-22 07:04:22 -0600

    FINALLY, automated (no more 'idx') prev/next post URLs and titles

--- .eleventy.js
+++ .eleventy.js
@@ -100,6 +100,25 @@ module.exports = function (eleventyConfig) {
 
   /* === END, webmentions stuff === */
 
+  /* === START, prev/next posts stuff === */
+  // https://github.com/11ty/eleventy/issues/529#issuecomment-568257426
+
+  eleventyConfig.addCollection("posts", function(collection) {
+    const coll = collection.getFilteredByTag("post")
+
+    for(let i = 0; i < coll.length; i++) {
+      const prevPost = coll[i-1]
+      const nextPost = coll[i+1]
+
+      coll[i].data["prevPost"] = prevPost
+      coll[i].data["nextPost"] = nextPost
+    }
+
+    return coll;
+  })
+
+  /* === END, prev/next posts stuff === */
+
   
   /* pathPrefix: "/"; */
   return {
