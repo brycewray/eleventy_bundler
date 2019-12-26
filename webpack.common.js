954b5a7f4f9b382d1c63a5f39a0ac39061b1338d
Bryce Wray <bwray@wraytx.com>
2019-12-26 08:15:31 -0600

    See previous commit (losing PostCSS)

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const Dotenv = require('dotenv-webpack')
-const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   entry: [
@@ -41,19 +40,6 @@ module.exports = {
             },
           },
           'css-loader',
-          {
-            loader: 'postcss-loader', 
-            options: {
-              ident: 'postcss',
-              plugins: () => [
-                postcssPresetEnv({
-                  autoprefixer: {
-                    grid: true
-                  }
-                }),
-              ],
-            },
-          },
           'sass-loader',
         ],
       },
