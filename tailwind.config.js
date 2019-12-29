b88d9c75e60e7eaeb70a92db3ae93e37130c581f
Bryce Wray <bwray@wraytx.com>
2019-12-29 14:01:02 -0600

    Eliminating unneeded and problematic settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,6 @@ module.exports = {
       codefont: ['Roboto Mono', 'monospace'],
     },
   },
-  variants: {
-    backgroundColor: ['dark', 'dark-hover', 'dark-group-hover'],
-    borderColor: ['dark', 'dark-focus', 'dark-focus-within'],
-    textColor: ['dark', 'dark-hover', 'dark-active'],
-  },
-  plugins: [
-    require('tailwindcss-dark-mode')()
-  ],
+  // variants: {},
+  // plugins: [],
 }
