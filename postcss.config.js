1b491162b4ba436b2bbe26df29e01aaa7360f256
Bryce Wray <bwray@wraytx.com>
2020-01-17 15:32:40 -0600

    Ongoing config tweaks

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,30 @@
+const postcssPresetEnv = require('postcss-preset-env')
+const cssnano = require('cssnano')
+const cssvariables = require('postcss-css-variables')
+const postcssPartialImport = require ('postcss-partial-import')
+const postcssNesting = require('postcss-nesting')
+
 module.exports = {
-  plugins: {
-    'postcss-preset-env': {
-      stage: 1,
-      autoprefixer: {
-        grid: true,
+  plugins: [
+    require('postcss-preset-env'),
+    postcssPresetEnv({
+      stage: 0,
+      browsers: [
+        'last 2 versions',
+        'IE 11',
+      ],
+      features: {
+        'nesting-rules': true,
+        'custom-properties': true,
       },
-      browsers: 'last 2 versions',
-    }, 
-    'postcss-nested': {},
-    'purgecss': {
-      extensions: [
-        'html', 'njk'
-      ], 
-    },
-  },
+    }),
+    postcssPartialImport({
+      root: './src/assets/css/',
+    }),
+    cssvariables({}),
+    postcssNesting({}),
+    process.env.NODE_ENV === 'production' 
+    ? cssnano({ preset: 'default' })
+    : null,
+  ],
 }
