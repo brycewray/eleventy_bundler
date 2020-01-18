1e7f777987b087e94fd56b5acf6a4827a9ec331a
Bryce Wray <bwray@wraytx.com>
2020-01-18 12:51:21 -0600

    Getting rid of PurgeCSS (more trouble than it's worth and saves only 2K in final size).

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,6 @@
 const merge = require("webpack-merge")
 const common = require("./webpack.common.js")
-const path = require('path')
-const glob = require('glob-all')
-const PurgecssPlugin = require('purgecss-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const purgeHtml = require('purgecss-from-html')
 
 class TailwindExtractor {
   static extract(content) {
@@ -14,18 +10,6 @@ class TailwindExtractor {
 module.exports = merge(common, {
   mode: "production",
   plugins: [
-    new PurgecssPlugin({
-      paths: glob.sync([
-        path.join(__dirname, './src/**/*.html'),
-        path.join(__dirname, './src/**/*.njk'),
-      ]),
-      extractors: [
-        {
-          extractor: purgeHtml,
-          extensions: ['html', 'njk']
-        },
-      ],
-    }),
     new OptimizeCssAssetsPlugin({
       cssProcessorPluginOptions: {
         preset: [
