fbd5fa9f60dfef1cdb9beb34c9ba7af9258f6cf0
Bryce Wray <bwray@wraytx.com>
2020-01-25 10:33:17 -0600

    Adding Mozjpeg to further improve image handling

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const merge = require("webpack-merge")
 const common = require("./webpack.common.js")
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default
+const imageminMozjpeg = require('imagemin-mozjpeg')
 
 class TailwindExtractor {
   static extract(content) {
@@ -28,14 +29,16 @@ module.exports = merge(common, {
         speed: 10,
         quality: 50-80,
       },
-      mozjpeg: {
-        quality: 60,
-        progressive: true,
-      },
       gifsicle: {
         optimizationLevel: 2,
         colors: 64,
       },
+      plugins: [
+        imageminMozjpeg({
+          quality: 60,
+          progressive: true,
+        }),
+      ],
     }),
   ],
 })
