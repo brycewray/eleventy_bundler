16d088f218520fc1f558a9cb69d16efa9a643e18
Bryce Wray <bwray@wraytx.com>
2020-01-29 20:57:29 -0600

    Revising to work with vanilla-lazyload

--- src/assets/utils/lazy-picture.js
+++ src/assets/utils/lazy-picture.js
@@ -6,7 +6,22 @@ shortcode takes the following form...
 
 module.exports = (urlBasic, ext, width, alt) => {
   return `
-  <picture><source class="lazyload blur-up containedImage" type="image/webp" srcset="/images/${urlBasic}-20.webp" data-srcset="/images/${urlBasic}-300.webp 300w, /images/${urlBasic}-600.webp 600w, /images/${urlBasic}-${width}.webp ${width}w" /><source class="lazyload blur-up containedImage" type="image/${ext}" srcset="/images/${urlBasic}-20.${ext}" data-srcset="/images/${urlBasic}-300.${ext} 300w, /images/${urlBasic}-600.${ext} 600w, /images/${urlBasic}-${width}.${ext} ${width}w" /><img class="lazyload blur-up containedImage" loading="lazy" src="/images/${urlBasic}-${width}.${ext}" alt="${alt}" /></picture>
-  <noscript><img class="containedImage" loading="lazy" src="/images/${urlBasic}-${width}.${ext}" alt="${alt}" /></noscript>
+  <picture class="noIE">
+  <source type="image/webp" 
+  data-srcset="/images/${urlBasic}-300.webp 300w, 
+  /images/${urlBasic}-600.webp 600w, 
+  /images/${urlBasic}-${width}.webp ${width}w" />
+  <img class="lazy noIE containedImage" 
+  src="/images/${urlBasic}-20.${ext}" alt="${alt}" 
+  data-src="/images/${urlBasic}-300.${ext}" 
+  data-srcset="/images/${urlBasic}-300.${ext} 300w, 
+  /images/${urlBasic}-600.${ext} 600w, 
+  /images/${urlBasic}-${width}.${ext} ${width}w" 
+  alt="${alt}" />
+  </picture>
+  <img class="IEonly" src="/images/${urlBasic}-${width}.${ext}" alt="${alt}" />
+  <noscript>
+  <img class="containedImage" loading="lazy" src="/images/${urlBasic}-${width}.${ext}" alt="${alt}" />
+  </noscript>
   `
 }
