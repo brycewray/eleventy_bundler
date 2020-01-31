0bc58e14e3fabf86a8b5dcc39279d534a1e93ea3
Bryce Wray <bwray@wraytx.com>
2020-01-30 20:44:04 -0600

    Cleanup to avoid superfluous image downloads in Chromium

--- src/assets/utils/lazy-picture.js
+++ src/assets/utils/lazy-picture.js
@@ -6,7 +6,13 @@ shortcode takes the following form...
 
 module.exports = (urlBasic, ext, width, alt) => {
   return `
-  <picture><source class="lazyload blur-up containedImage" type="image/webp" srcset="/images/${urlBasic}-20.webp" data-srcset="/images/${urlBasic}-300.webp 300w, /images/${urlBasic}-600.webp 600w, /images/${urlBasic}-${width}.webp ${width}w" /><source class="lazyload blur-up containedImage" type="image/${ext}" srcset="/images/${urlBasic}-20.${ext}" data-srcset="/images/${urlBasic}-300.${ext} 300w, /images/${urlBasic}-600.${ext} 600w, /images/${urlBasic}-${width}.${ext} ${width}w" /><img class="lazyload blur-up containedImage" loading="lazy" src="/images/${urlBasic}-${width}.${ext}" alt="${alt}" /></picture>
-  <noscript><img class="containedImage" loading="lazy" src="/images/${urlBasic}-${width}.${ext}" alt="${alt}" /></noscript>
+  <picture>
+  <source type="image/webp" srcset="/images/${urlBasic}-20.webp" data-srcset="/images/${urlBasic}-300.webp 300w, /images/${urlBasic}-600.webp 600w, /images/${urlBasic}-${width}.webp ${width}w" />
+  <source type="image/${ext}" srcset="/images/${urlBasic}-20.${ext}" data-srcset="/images/${urlBasic}-300.${ext} 300w, /images/${urlBasic}-600.${ext} 600w, /images/${urlBasic}-${width}.${ext} ${width}w" />
+  <img class="lazyload blur-up containedImage" src="/images/${urlBasic}-${width}.${ext}" alt="${alt}" />
+  </picture>
+  <noscript>
+  <img class="containedImage" loading="lazy" src="/images/${urlBasic}-${width}.${ext}" alt="${alt}" />
+  </noscript>
   `
 }
