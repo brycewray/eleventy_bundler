24784dda00f2fe573bea090bb3593d0e45255d1b
Bryce Wray <bwray@wraytx.com>
2020-03-12 20:58:02 -0500

    Restoring Talkyard commenting

--- src/_includes/layouts/posts/singlepost.11ty.js
+++ src/_includes/layouts/posts/singlepost.11ty.js
@@ -47,40 +47,17 @@ exports.render = function (data) {
     </article>
   </div>
 
-  ${
-    data.title !== "Home page" && data.title !== "Posts"
-    ? `
-    <!-- from https://github.com/maxboeck/eleventy-webmentions/blob/master/_includes/webmention.njk -->
-  
-    <article class="webmention h-cite" id="webmention-${data.webmention}['wm-id']">
-      <div class="webmention__meta">
-        ${
-          (data.webmention.author !== null) 
-          ? `<a class="webmention__author p-author h-card u-url" href="${data.webmention.url}" target="_blank" rel="noopener noreferrer">
-            ${(data.webmention.author.photo !== null)
-              ? `<img class="webmention__author__photo u-photo" src="${data.webmention.author.photo}" alt="${data.webmention.author.name}">`
-              : `<img class="webmention__author__photo" src="/images/webmention-avatar-default.svg" alt="">`
-            }
-            <strong class="p-name">${data.webmention.author.name}</strong>
-          </a>`
-          : `<span class="webmention__author">
-            <img class="webmention__author__photo" src="/images/webmention-avatar-default.svg" alt="">
-            <strong>Anonymous</strong>
-          </span>`
-        }
-    
-        ${(data.webmention.published !== null)
-          ? `<time class="webmention__pubdate dt-published" datetime="${data.webmention.published}">${data.webmention.published}</time><!-- orig was dd LLL yyyy -->`
-          : ``
-        }
-      </div>
-      <div class="webmention__content p-content">
-        ${data.webmention.content.value}
-      </div>
-    </article>
-    `
-    : ``
-  }
+  <div class="container container-comments">
+    <p>&nbsp;</p>
+    <!-- Comments code -->
+    <script>talkyardServerUrl="${data.siteparams.talkyardServerUrl}";</script>
+    <script async defer src="${data.siteparams.talkyardScriptUrl}"></script>
+    <div class="talkyard-comments" data-discussion-id="${data.discussionId}" data-iframe-title="Comments section on brycewray.com, powered by Talkyard" style="margin-top: 3em;">
+      <noscript>Please enable Javascript to view comments.</noscript>
+      <!-- commenting this out because it's in the footer ... <p class="legal text-muted" style="margin-top: 2em;">Comments powered by <a href="https://www.talkyard.io">Talkyard</a>.</p> -->
+    </div>
+    <!-- end, Comments code -->
+  </div>
   
   ${data.title != "The obligatory About Me page"
     ? `<div class="bg-dark">
