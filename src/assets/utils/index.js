778d883c668bb1220e2ec10cb6de9eb20a4dda3b
Bryce Wray <bwray@wraytx.com>
2020-04-11 09:54:45 -0500

    Fixing a typo

--- src/assets/utils/index.js
+++ src/assets/utils/index.js
@@ -2,7 +2,7 @@
 // https://gitlab.com/reubenlillie/eleventy-dot-js-blog/-/blob/master/_includes/shortcodes/index.js
 // ... as of 2020-04-10
 
-var postlists = require('./postslist')
+var postslists = require('./postslist')
 var paginationNav = require('./pagination-nav')
 
 module.exports = eleventyConfig => {
