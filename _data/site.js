0806b3635acaa1530fc58e3c3be9eb6e96be574b
Bryce Wray <bwray@wraytx.com>
2020-04-10 23:24:10 -0500

    Site vars

---
+++ _data/site.js
@@ -0,0 +1,35 @@
+// Based shamelessly on Reuben Lillie's code:
+// https://gitlab.com/reubenlillie/eleventy-dot-js-blog/-/blob/master/_data/site.js
+// (as of 2020-04-10)
+
+ module.exports = {
+   baseURL: 'https://brycewray.com',
+   defaultLocale: 'en-US',
+   en: {
+     locale: 'en-US',
+     title: 'BryceWray.com',
+     tagline: 'Observations, opinions, geekery.',
+     separator: ' • ',
+     pagination: {
+       navLabel: 'Pagination',
+       pageLabel: 'Page',
+       first: '1',
+       firstLabel: '1',
+       previous: 'Previous',
+       next: 'Next',
+       last: 'End',
+       lastLabel: 'Last page'
+     },
+     postsArchive: {
+       headline: 'Posts',
+       prompt: 'Visit the posts',
+       url: '/posts/'
+     },
+     dateOptions: {
+       year: 'numeric',
+       month: 'long',
+       day: 'numeric',
+       timeZone: 'America/Chicago'
+     }
+   }
+ }
