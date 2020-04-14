d97e87cd3a2b14a4984492a590549418f2449b0c
Bryce Wray <bwray@wraytx.com>
2020-04-14 14:41:13 -0500

    Making webmentionList shortcode work

--- src/_includes/ofotigrid.js
+++ src/_includes/ofotigrid.js
@@ -5,6 +5,7 @@ var headTag = require('./layouts/partials/head') // head.js
 var siteHeader = require('./layouts/partials/header') // etc. ...
 var siteFooter = require('./layouts/partials/footer')
 var titleTag = require('./layouts/partials/title-tag')
+var webmentionList = require('./webmentionlist.js')
 
 module.exports = function(eleventyConfig) {
 
@@ -12,6 +13,7 @@ module.exports = function(eleventyConfig) {
   siteHeader(eleventyConfig)
   siteFooter(eleventyConfig)
   titleTag(eleventyConfig)
+  webmentionList(eleventyConfig)
 
   return
   
