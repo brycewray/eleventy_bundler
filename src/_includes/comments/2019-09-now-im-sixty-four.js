89f472d4e084b0310f1752402c03d949441f253d
Bryce Wray <bwray@wraytx.com>
2020-04-14 09:33:48 -0500

    Reverting to previous while figuring this out

--- src/_includes/comments/2019-09-now-im-sixty-four.js
+++ src/_includes/comments/2019-09-now-im-sixty-four.js
@@ -1,16 +1,14 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addShortcode('2019-09-now-im-sixty-four', function() {
-    return `
-    <div class="webmentions">
-      <h3 class="ctr">Comments</h3>
-      <p class="legal ctr">
-        (Imported from previous comments host, <a href="https://talkyard.io">Talkyard</a>.)
-      </p>
-    
-      <p class="commentOpen"><strong>Cap Pike</strong> <em>2019-09-07</em></p>
-      <p class="commentBody">People that use Word that don't &quot;get&quot; styles and formatting in general are generally just annoying. Like people who drive that don't bother with turn signals.<br />
-        ...and then there's Excel...</p>
-    </div>
-    `
-  })
-}
+// .11ty.js version of 2019-09-now-im-sixty-four.njk
+
+module.exports = `
+<div class="webmentions">
+  <h3 class="ctr">Comments</h3>
+  <p class="legal ctr">
+    (Imported from previous comments host, <a href="https://talkyard.io">Talkyard</a>.)
+  </p>
+
+  <p class="commentOpen"><strong>Cap Pike</strong> <em>2019-09-07</em></p>
+  <p class="commentBody">People that use Word that don't &quot;get&quot; styles and formatting in general are generally just annoying. Like people who drive that don't bother with turn signals.<br />
+    ...and then there's Excel...</p>
+</div>
+`
